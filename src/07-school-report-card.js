e62320f172bfb2110cbc1f9bf4399427748a7cc1
abdcantcode <abdullah72ftw@gmail.com>
2026-02-13 01:33:09 +0530

    7th done

--- src/07-school-report-card.js
+++ src/07-school-report-card.js
@@ -41,5 +41,47 @@
  *   // => { name: "Priya", totalMarks: 63, percentage: 31.5, grade: "F", ... }
  */
 export function generateReportCard(student) {
-  // Your code here
+  if((typeof student!=='object'||Array.isArray(student)||student===null)
+    || (typeof student.name!=='string'||student.name.trim().length===0) 
+  || (typeof student.marks!=='object'||Array.isArray(student.marks)||student.marks===null)
+  || (Object.keys(student.marks).length===0)
+  || (Object.values(student.marks).some(e=>typeof e!=='number'||e<0 || e>100 ))
+) return null;
+  let name=student.name;
+  let subjectmarksarray=Object.values(student.marks);
+  let totalMarks=subjectmarksarray.reduce((acc,num)=>(acc+num),0)
+  let numSubjects=subjectmarksarray.length;
+  let percentage=parseFloat(((totalMarks / (numSubjects * 100)) * 100).toFixed(2))
+  let subandmarks=Object.entries(student.marks);
+  let grade="";
+  if(percentage<40) grade="F";
+  else if(percentage>= 40 && percentage<59) grade="D";
+  else if(percentage>=60 && percentage<69) grade="C";
+  else if(percentage>=70 && percentage<79) grade="B";
+  else if(percentage>=80 && percentage<89) grade="A";
+  else if(percentage>=90 && percentage<=100) grade="A+";
+  let passedSubjects = Object.entries(student.marks)
+ .filter(([subject, marks]) => marks >= 40)
+  .map(([subject]) => subject);
+  let failedSubjects = Object.entries(student.marks)
+  .filter(([subject, marks]) => marks < 40)
+  .map(([subject]) => subject);
+  let highestmarkindex=subjectmarksarray.indexOf(Math.max(...subjectmarksarray));
+  let lowestmarkindex=subjectmarksarray.indexOf(Math.min(...subjectmarksarray));
+  let subjectnamesarray=Object.keys(student.marks);
+  let highestMarks=subjectnamesarray[highestmarkindex];
+  let lowestMarks=subjectnamesarray[lowestmarkindex];
+  return {
+  name,
+  totalMarks,
+  percentage,
+  grade,
+  highestSubject: highestMarks,
+  lowestSubject: lowestMarks,
+  passedSubjects,
+  failedSubjects,
+  subjectCount: subjectnamesarray.length
+};
+
+
 }
